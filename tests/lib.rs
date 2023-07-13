c35499eb10775b1c6cf549648ca914e15bd3ec35
Marek Miller <marek.miller@protonmail.com>
2023-07-14 00:11:15 +0200

    Add test crate

---
+++ tests/lib.rs
@@ -0,0 +1,8 @@
+pub fn does_it_work(
+    left: usize,
+    right: usize,
+) -> usize {
+    left + right
+}
+
+mod unit;
---
+++ tests/unit/check.rs
@@ -0,0 +1,7 @@
+use crate::does_it_work;
+
+#[test]
+fn it_works() {
+    let result = does_it_work(2, 2);
+    assert_eq!(result, 4);
+}
